dfe1d2114ca4a4fcf67fd1c21e2afd0720a153b0
Ozer-Sama <83515041+Oz174@users.noreply.github.com>
2022-12-31 23:34:52 +0200

    new test_bench for multiplier

--- fp_tb_mul.sv
+++ fp_tb_mul.sv
@@ -1,37 +1,27 @@
 module fp_tb();
-real a = 1.5;
-real b = 1.5;
-reg clk = 1'b0;
-reg load = 1'b0;
+shortreal a = 1.5;
+shortreal b = 1.5;
 localparam No = 32;
 //reg op = 1'b0;
 wire [No-1:0] out;
-wire valid;
 initial begin
-    #20 load = 1'b1;
     //op = 1'b0;
-    a = 1.37432432;
-    b = 1.0;
-    #40 load = 1'b0;
-    
-    #60 load = 1'b1;
+    a = -0.01;
+    b = 0.1;
     //op = 1'b1;
-    a = 1.39040394;
-    b = 0.5;
-    #80 load = 1'b0;
-    
-    #100 load = 1'b1;
+    a = 6.446;
+    b = 4.542;
+	 
     //op = 1'b1;
-    a = $bitstoreal(32'hffffffff);
+    a = $bitstoshortreal(32'hffffffff);
     b = 7.2;
-    #120 load = 1'b0;
 end
 
 always @(out)
 begin 
-$display("%f * %f = %f", a, b, $bitstoreal(out));
+$display("%f * %f = %f", a, b, $bitstoshortreal(out));
 end
 
-FP_Multiplier #(.N(No)) fp_mul_ins (.A($realtobits(a)),.B($realtobits(b)),.Result(out));
-//float_adder_subtractor ins (.inA($shortrealtobits(a)),.inB($shortrealtobits(b)),.clk(clk),.op(op),.load(load),.out(out),.valid(valid));
+FP_Multiplier #(.N(No)) fp_mul_ins (.A($shortrealtobits(a)),.B($shortrealtobits(b)),.Result(out));
+//float_adder_subtractor ins (.inA($shortshortrealtobits(a)),.inB($shortshortrealtobits(b)),.clk(clk),.op(op),.load(load),.out(out),.valid(valid));
 endmodule
