23895699bb356651f163565a94eff4f0b3570bbf
Noureldin7 <noureldin.salah01@eng-st.cu.edu.eg>
2022-12-31 22:55:23 +0200

    TestBench++

--- fp_tb.sv
+++ fp_tb.sv
@@ -9,21 +9,45 @@ wire valid;
 initial begin
     #20 load = 1'b1;
     op = 1'b0;
+    a = 0;
+    b = 1.5;
+    #20 load = 1'b0;
+	 
+	 #20 load = 1'b1;
+    op = 1'b1;
+    a = -3;
+    b = 1.5;
+    #20 load = 1'b0;
+	 
+	 #20 load = 1'b1;
+    op = 1'b0;
+    a = 1.12646;
+    b = 1.523;
+    #20 load = 1'b0;
+	 
+	 #20 load = 1'b1;
+    op = 1'b0;
     a = 1.5;
     b = 1.5;
-    #40 load = 1'b0;
+    #20 load = 1'b0;
     
-    #60 load = 1'b1;
+    #20 load = 1'b1;
     op = 1'b1;
     a = 12.4;
     b = 7.2;
-    #80 load = 1'b0;
+    #20 load = 1'b0;
     
-    #100 load = 1'b1;
+	 #20 load = 1'b1;
+    op = 1'b1;
+    a = 0;
+    b = 1.5;
+    #20 load = 1'b0;
+	 
+    #20 load = 1'b1;
     op = 1'b1;
     a = $bitstoshortreal(32'hffffffff);
     b = 7.2;
-    #120 load = 1'b0;
+    #20 load = 1'b0;
 end
 always @(posedge valid) begin
     $display("%f %s %f = %f", a, (op==1'b0)?"+":"-", b, $bitstoshortreal(out));
